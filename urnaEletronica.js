196a30b7d2fc08758f7bb976f0ad667df22d9f30
SauerNelson <sauer_dev@outlook.com>
2023-10-06 19:42:25 +0000

    feat:criando pagina js.

---
+++ urnaEletronica.js
@@ -0,0 +1,22 @@
+// Estrutura de repetição: do-while (faça-enquanto)
+
+let contador = 0
+do {
+    console.log('Instrução')
+    contador += 1;
+} while (contador <= 10)
+
+// Estrutura de repetição: while (enquanto)
+
+while (opcao !== 0) {
+    console.log('Instrução1')
+    console.log('Instrução2')
+    console.log('Instrução3')
+    console.log('Instrução4')
+    console.log('Instrução5')
+    console.log('Instrução6')
+    console.log('Instrução7')
+    console.log('Instrução8')
+    console.log('Instrução9')
+    console.log('Instrução10')
+}
